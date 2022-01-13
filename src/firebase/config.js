8a7eb44c98533ff0d55740f8cb5c77760a1ebc08
GrzegorzBanaszak <grzegorz.banaszak95@gmail.com>
2022-01-13 21:55:33 +0100

    Add firebase config

---
+++ src/firebase/config.js
@@ -0,0 +1,31 @@
+// Import the functions you need from the SDKs you need
+
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.2/firebase-app.js";
+
+  // TODO: Add SDKs for Firebase products that you want to use
+
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+
+  // Your web app's Firebase configuration
+
+  const firebaseConfig = {
+
+    apiKey: "AIzaSyBngpNMZcL4tOlEfYYIXKRUsfuTkc1eEZs",
+
+    authDomain: "fir-playground-199c9.firebaseapp.com",
+
+    projectId: "fir-playground-199c9",
+
+    storageBucket: "fir-playground-199c9.appspot.com",
+
+    messagingSenderId: "1046679345392",
+
+    appId: "1:1046679345392:web:1bb8ed80177f535e124a35"
+
+  };
+
+
+  // Initialize Firebase
+
+  const app = initializeApp(firebaseConfig);
